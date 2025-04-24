ffc196fcee9fcf8941707d1209b6f9718ee3fd2d
RadotJar <reilly.hl12@gmail.com>
2025-04-24 21:38:42 +0930

    Fix decrement bug and add spell support

--- scripts/dm-assistant.js
+++ scripts/dm-assistant.js
@@ -28,15 +28,19 @@ function addTorch( event ) {
     durationsContainer.appendChild(newTorch);
 }
 
-function addSpell() {
-    const spellDuration = document.createElement('span');
-    spellDuration.className = '.turns-remaining';
-    spellDuration.innerText = '10';
+function addSpell( event ) {
+    const characterName = event.target.characterName.value;
+    const spellName = event.target.spellName.value;
+    const spellDuration = event.target.spellDuration.value;
+
+    const durationSpan = document.createElement('span');
+    durationSpan.className = '.turns-remaining';
+    durationSpan.innerText = spellDuration;
 
     const newSpell = document.createElement('button');
-    newSpell.innerText = 'Spell ';
+    newSpell.innerText = characterName + '\'s ' + spellName + ' ';
     newSpell.className = 'duration-spell'
-    newSpell.appendChild(spellDuration);
+    newSpell.appendChild(durationSpan);
 
     const durationsContainer = document.getElementById('durations');
     durationsContainer.appendChild(newSpell);
@@ -48,25 +52,34 @@ function updateHour() {
     hour.innerText = updatedHour;
 }
 
-// There is a bug
+// This is gross
 function updateDurations() {
     const durationsContainer = document.getElementById('durations');
     const durations = durationsContainer.children;
+
     for ( const duration of durations ) {
         const turnsRemainingElm = duration.getElementsByClassName('.turns-remaining')[0];
         const turnsRemaining = Number(turnsRemainingElm.innerText);
         const updatedTurns = turnsRemaining - 1;
-
-        if ( updatedTurns < 1 ) {
-            duration.remove()
-            continue;
-        }
-
         turnsRemainingElm.innerText = updatedTurns;
     }
+
+    for ( const duration of durations ) {
+        const turnsRemainingElm = duration.getElementsByClassName('.turns-remaining')[0];
+        const turnsRemaining = Number(turnsRemainingElm.innerText);
+        if ( turnsRemaining < 1 ) {
+            duration.remove()
+        }
+    }
+
 }
 
 function openTorchModal() {
     const modal = document.getElementById('torch-modal');
+    modal.showModal();
+}
+
+function openSpellModal() {
+    const modal = document.getElementById('spell-modal');
     modal.showModal();
 }
